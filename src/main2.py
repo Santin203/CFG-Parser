8d8d0c96346ae3ce12df754627b48814dd7e30f3
Santiago Jimenez <santiago.jimenez.casas@gmail.com>
2024-03-21 21:12:38 -0600

    Testing different cyk algorithms

--- src/main2.py
+++ src/main2.py
@@ -129,3 +129,17 @@ if __name__ == '__main__':
     r = read_input()[0]
     ta = cyk_alg(v, t, r)
     show_result(ta, r)
+    
+# Copyright 2019 mmheydari97
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
---
+++ src/main3.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import os
+
+def is_in_cartesian_prod(x, y, r):
+    return r in [i+j for i in x.split(',') for j in y.split(',')]
+
+def read_grammar(filename="./grammar.txt"):
+    grammar = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            key, value = line.strip().split(' -> ')
+            if key in grammar:
+                grammar[key].append(value)
+            else:
+                grammar[key] = [value]
+    return grammar
+
+
+
+def accept_CYK(w, G, S):
+    if w == 'ε':
+        return 'ε' in G[S]
+    n = len(w)
+    DP_table = [['']*n for _ in range(n)]
+    for i in range(n):
+        for lhs in G.keys():
+            for rhs in G[lhs]:
+                 if w[i] == rhs: # rules of the form A -> a
+                    DP_table[i][i] = lhs if not DP_table[i][i] else DP_table[i][i] + ',' + lhs
+                    
+    for l in range(2, n+1):       # span
+        for i in range(n-l+1):    # start
+            j = i+l-1             # right
+            for k in range(i, j): # partition
+                for lhs in G.keys():
+                    for rhs in G[lhs]:
+                        if len(rhs) == 2: #rules of form A -> BC
+                            if is_in_cartesian_prod(DP_table[i][k], DP_table[k+1][j], rhs):
+                                if not lhs in DP_table[i][j]:
+                                    DP_table[i][j] = lhs if not DP_table[i][j] else DP_table[i][j] + ',' + lhs
+
+    return S in DP_table[0][n-1]  
+
+# let's define the grammar productions and symbols first 
+NTs = ['S', 'A', 'B', 'C', 'D']
+Ts = ['a', 'b']
+rules = ['S -> AB | BC', 'A -> BA | a', 'B -> CC | b', 'C -> AB | a'] #, 'D -> ϵ']
+G = read_grammar()
+print(G)
+# {'S': ['AB', 'BC'], 'A': ['BA', 'a'], 'B': ['CC', 'b'], 'C': ['AB', 'a']}
+
+# now check if the string w is a member of G
+print(accept_CYK('n*n/n^n', G, 'E')) # True
